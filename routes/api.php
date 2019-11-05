ee8e2048b31c6080d101821cfce9e756146498f5
Tatsiana <t-tanja@bk.ru>
2019-11-05 16:40:08 +0300

    api

--- routes/api.php
+++ routes/api.php
@@ -29,6 +29,8 @@ foreach(\App\Http\Controllers\ApiController::$OPERATIONS as $operation ) {
 }
 
 
+
+
 Route::post('/upload','UploadController@upload')->middleware('token:upload','mobile');
 
 Route::get('/channels','ApiController@channels')->middleware('mobile');
